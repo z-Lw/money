1a18fd8356dc9452a6c8fa5b26d5d1251191c065
z-Lw <553870855@qq.com>
2021-02-26 16:32:43 +0800

    将tag string类型改成{id，name}

--- src/model/tagListModel.ts
+++ src/model/tagListModel.ts
@@ -1,19 +1,24 @@
 const localStorageKeyName='tagList';
+type Tag ={
+  id: string;
+  name: string;
+}
 type TagListModel={
-  data: string[];
-  fetch: () => string[];
+  data: Tag[];
+  fetch: () => Tag[];
   create: (name: string) => 'success'| 'duplicated';
   save: () => void;
 }
 const tagListModel: TagListModel={
-  data:[],
+  data:[{id:'',name:''}],
   fetch(){
     this.data= JSON.parse(window.localStorage.getItem(localStorageKeyName) ||'[]')
     return this.data
   },
   create(name ){
-    if(this.data.indexOf(name)>=0){return 'duplicated';}
-    this.data.push(name);
+    const names = this.data.map(item=>item.name)
+    if(names.indexOf(name)>=0){return 'duplicated';}
+    this.data.push({id:name,name:name});
     this.save();
     return 'success';
   },
