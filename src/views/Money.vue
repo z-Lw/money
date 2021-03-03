1526f9e6569dce31de9648660302911a7bf31203
z-Lw <553870855@qq.com>
2021-03-03 11:27:31 +0800

    将create 封装到recordListModel

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -48,13 +48,11 @@
       this.record.notes=value
     }
     saveRecord(){
-      const record2: RecordItem=recordListModel.clone(this.record)
-      record2.createdAt=new Date()
-      this.recordList.push(record2)
+      recordListModel.create(this.record)
     }
     @Watch('recordList')
     onRecordListChange(){
-      recordListModel.save(this.recordList);
+      recordListModel.save();
     }
     //   this.record.type=value
     // onUpdateType(value:string){
